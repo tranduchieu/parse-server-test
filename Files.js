87cf4c090e08bf2ef2cfe0015d50986334ceb74f
Hieu Tran <hieu.tranduc@gmail.com>
2016-07-24 13:40:57 +0700

    File change

--- Files.js
+++ Files.js
@@ -1,1 +1,5 @@
 // Config https://github.com/ParsePlatform/parse-server/wiki/Configuring-File-Adapters#configuring-s3adapter
+import Parse from 'parse/node';
+
+var base64 = "V29ya2luZyBhdCBQYXJzZSBpcyBncmVhdCE=";
+var file = new Parse.File("myfile.txt", { base64: base64 });
